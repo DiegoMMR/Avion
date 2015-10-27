f4fd5b4d2064f9ec8d23ccacb5e86a5414dbbaa0
Diego M. Rivera <diego.mz.rv@gmail.com>
2015-10-26 23:46:50 -0600

    Inicio

---
+++ Avion/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace Avion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Avion = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Rusia = new System.Windows.Forms.PictureBox();
+            this.Guatemala = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Avion)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Rusia)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Guatemala)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = global::Avion.Properties.Resources.mapamundi;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Controls.Add(this.Avion);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.Rusia);
+            this.panel1.Controls.Add(this.Guatemala);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(768, 474);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // Avion
+            // 
+            this.Avion.BackColor = System.Drawing.Color.Transparent;
+            this.Avion.BackgroundImage = global::Avion.Properties.Resources.Avion;
+            this.Avion.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Avion.Location = new System.Drawing.Point(98, 343);
+            this.Avion.Name = "Avion";
+            this.Avion.Size = new System.Drawing.Size(59, 60);
+            this.Avion.TabIndex = 5;
+            this.Avion.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(659, 435);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(18, 440);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Y = 0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(18, 418);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "X = 0";
+            // 
+            // Rusia
+            // 
+            this.Rusia.BackColor = System.Drawing.Color.Transparent;
+            this.Rusia.BackgroundImage = global::Avion.Properties.Resources.Punto;
+            this.Rusia.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Rusia.InitialImage = null;
+            this.Rusia.Location = new System.Drawing.Point(503, 124);
+            this.Rusia.Name = "Rusia";
+            this.Rusia.Size = new System.Drawing.Size(20, 20);
+            this.Rusia.TabIndex = 1;
+            this.Rusia.TabStop = false;
+            this.Rusia.Click += new System.EventHandler(this.Rusia_Click);
+            // 
+            // Guatemala
+            // 
+            this.Guatemala.BackColor = System.Drawing.Color.Transparent;
+            this.Guatemala.BackgroundImage = global::Avion.Properties.Resources.Punto;
+            this.Guatemala.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Guatemala.InitialImage = null;
+            this.Guatemala.Location = new System.Drawing.Point(196, 253);
+            this.Guatemala.Name = "Guatemala";
+            this.Guatemala.Size = new System.Drawing.Size(20, 20);
+            this.Guatemala.TabIndex = 0;
+            this.Guatemala.TabStop = false;
+            this.Guatemala.Click += new System.EventHandler(this.Guatemala_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(792, 498);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Avion)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Rusia)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Guatemala)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox Guatemala;
+        private System.Windows.Forms.PictureBox Rusia;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox Avion;
+    }
+}
+
---
+++ Avion/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Avion
+{
+
+
+    public partial class Form1 : Form
+    {
+        int Proceso = 0;
+        int X, Y;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+         
+
+            
+        }
+
+        private void Guatemala_Click(object sender, EventArgs e)
+        {
+            //Guatemala.Location.X (Y) obtiene la pocion donde esta el punto rojo
+            label1.Text = "X = " + Guatemala.Location.X;
+            label2.Text = "Y = " + Guatemala.Location.Y;
+            //solo para poder guardar los datos de una forma mas corta
+            X =  Guatemala.Location.X;
+            Y =  Guatemala.Location.Y;
+            
+            //coloca el Avion el la posicion de Guatemala
+            Avion.Location = new Point(X, Y);
+
+            
+        }
+
+        private void Rusia_Click(object sender, EventArgs e)
+        {
+            label1.Text = "X = " + Rusia.Location.X;
+            label2.Text = "Y = " + Rusia.Location.Y;
+            X = Rusia.Location.X;
+            Y = Rusia.Location.Y;
+
+            Avion.Location = new Point(X, Y);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // este proceso puede servir para ingrsar el inicio, destino y accionar el programa de movimiento
+            if (Proceso == 2)
+            {
+                button1.Text = "Vamos!";
+                Proceso = 3;
+            }
+
+            if (Proceso == 1)
+            {
+                button1.Text = "Destino";
+                Proceso = 2;
+            }
+
+            if (Proceso == 0)
+            {
+                button1.Text = "Inicio";
+                Proceso = 1;
+            }
+
+         
+
+          
+
+        }
+
+    }
+}
