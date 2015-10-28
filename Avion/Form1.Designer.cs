a66ba76c7bc42b0ee65247e6c1a1036777a3058f
Diego M. Rivera <diego.mz.rv@gmail.com>
2015-10-27 19:38:03 -0600

    Ya casi esta
    Validenlo porfa, pero para mi ya solo hay que agregarle mas paises

--- Avion/Form1.Designer.cs
+++ Avion/Form1.Designer.cs
@@ -29,58 +29,97 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.panel1 = new System.Windows.Forms.Panel();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Panel1 = new System.Windows.Forms.Panel();
+            this.Texto = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Tiempo = new System.Windows.Forms.Label();
             this.Avion = new System.Windows.Forms.PictureBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.Control = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.Rusia = new System.Windows.Forms.PictureBox();
             this.Guatemala = new System.Windows.Forms.PictureBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.Tiempo = new System.Windows.Forms.Label();
-            this.panel1.SuspendLayout();
+            this.TextoPrtida = new System.Windows.Forms.Label();
+            this.TextoDestino = new System.Windows.Forms.Label();
+            this.Panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.Avion)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.Rusia)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.Guatemala)).BeginInit();
             this.SuspendLayout();
             // 
-            // panel1
+            // Panel1
             // 
-            this.panel1.BackgroundImage = global::Avion.Properties.Resources.mapamundi;
-            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.panel1.Controls.Add(this.Tiempo);
-            this.panel1.Controls.Add(this.Avion);
-            this.panel1.Controls.Add(this.button1);
-            this.panel1.Controls.Add(this.label2);
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Controls.Add(this.Rusia);
-            this.panel1.Controls.Add(this.Guatemala);
-            this.panel1.Location = new System.Drawing.Point(12, 12);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(768, 474);
-            this.panel1.TabIndex = 0;
-            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.Panel1.BackgroundImage = global::Avion.Properties.Resources.mapamundi;
+            this.Panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Panel1.Controls.Add(this.Avion);
+            this.Panel1.Controls.Add(this.TextoDestino);
+            this.Panel1.Controls.Add(this.TextoPrtida);
+            this.Panel1.Controls.Add(this.Texto);
+            this.Panel1.Controls.Add(this.label3);
+            this.Panel1.Controls.Add(this.Tiempo);
+            this.Panel1.Controls.Add(this.Control);
+            this.Panel1.Controls.Add(this.label2);
+            this.Panel1.Controls.Add(this.label1);
+            this.Panel1.Controls.Add(this.Guatemala);
+            this.Panel1.Controls.Add(this.Rusia);
+            this.Panel1.Location = new System.Drawing.Point(12, 12);
+            this.Panel1.Name = "Panel1";
+            this.Panel1.Size = new System.Drawing.Size(768, 474);
+            this.Panel1.TabIndex = 0;
+            this.Panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // Texto
+            // 
+            this.Texto.AutoSize = true;
+            this.Texto.BackColor = System.Drawing.Color.Transparent;
+            this.Texto.Location = new System.Drawing.Point(308, 439);
+            this.Texto.Name = "Texto";
+            this.Texto.Size = new System.Drawing.Size(109, 13);
+            this.Texto.TabIndex = 8;
+            this.Texto.Text = "Haz clic en comenzar";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Location = new System.Drawing.Point(62, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "angulo = 0";
+            // 
+            // Tiempo
+            // 
+            this.Tiempo.AutoSize = true;
+            this.Tiempo.BackColor = System.Drawing.Color.Transparent;
+            this.Tiempo.Location = new System.Drawing.Point(21, 13);
+            this.Tiempo.Name = "Tiempo";
+            this.Tiempo.Size = new System.Drawing.Size(13, 13);
+            this.Tiempo.TabIndex = 6;
+            this.Tiempo.Text = "0";
             // 
             // Avion
             // 
             this.Avion.BackColor = System.Drawing.Color.Transparent;
             this.Avion.BackgroundImage = global::Avion.Properties.Resources.Avion;
             this.Avion.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.Avion.Location = new System.Drawing.Point(98, 343);
+            this.Avion.Location = new System.Drawing.Point(100, 366);
             this.Avion.Name = "Avion";
-            this.Avion.Size = new System.Drawing.Size(59, 60);
+            this.Avion.Size = new System.Drawing.Size(35, 35);
             this.Avion.TabIndex = 5;
             this.Avion.TabStop = false;
             // 
-            // button1
+            // Control
             // 
-            this.button1.Location = new System.Drawing.Point(659, 435);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "Inicio";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.Control.Location = new System.Drawing.Point(659, 435);
+            this.Control.Name = "Control";
+            this.Control.Size = new System.Drawing.Size(75, 23);
+            this.Control.TabIndex = 4;
+            this.Control.Text = "Comenzar";
+            this.Control.UseVisualStyleBackColor = true;
+            this.Control.Click += new System.EventHandler(this.button1_Click);
             // 
             // label2
             // 
@@ -108,7 +147,7 @@
             this.Rusia.BackgroundImage = global::Avion.Properties.Resources.Punto;
             this.Rusia.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.Rusia.InitialImage = null;
-            this.Rusia.Location = new System.Drawing.Point(503, 124);
+            this.Rusia.Location = new System.Drawing.Point(510, 127);
             this.Rusia.Name = "Rusia";
             this.Rusia.Size = new System.Drawing.Size(20, 20);
             this.Rusia.TabIndex = 1;
@@ -121,7 +160,7 @@
             this.Guatemala.BackgroundImage = global::Avion.Properties.Resources.Punto;
             this.Guatemala.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.Guatemala.InitialImage = null;
-            this.Guatemala.Location = new System.Drawing.Point(196, 253);
+            this.Guatemala.Location = new System.Drawing.Point(195, 259);
             this.Guatemala.Name = "Guatemala";
             this.Guatemala.Size = new System.Drawing.Size(20, 20);
             this.Guatemala.TabIndex = 0;
@@ -132,26 +171,37 @@
             // 
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
-            // Tiempo
+            // TextoPrtida
             // 
-            this.Tiempo.AutoSize = true;
-            this.Tiempo.BackColor = System.Drawing.Color.Transparent;
-            this.Tiempo.Location = new System.Drawing.Point(21, 13);
-            this.Tiempo.Name = "Tiempo";
-            this.Tiempo.Size = new System.Drawing.Size(13, 13);
-            this.Tiempo.TabIndex = 6;
-            this.Tiempo.Text = "0";
+            this.TextoPrtida.AutoSize = true;
+            this.TextoPrtida.BackColor = System.Drawing.Color.Transparent;
+            this.TextoPrtida.Location = new System.Drawing.Point(18, 176);
+            this.TextoPrtida.Name = "TextoPrtida";
+            this.TextoPrtida.Size = new System.Drawing.Size(46, 13);
+            this.TextoPrtida.TabIndex = 9;
+            this.TextoPrtida.Text = "Partida: ";
+            // 
+            // TextoDestino
+            // 
+            this.TextoDestino.AutoSize = true;
+            this.TextoDestino.BackColor = System.Drawing.Color.Transparent;
+            this.TextoDestino.Location = new System.Drawing.Point(18, 189);
+            this.TextoDestino.Name = "TextoDestino";
+            this.TextoDestino.Size = new System.Drawing.Size(49, 13);
+            this.TextoDestino.TabIndex = 10;
+            this.TextoDestino.Text = "Destino: ";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(792, 498);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.Panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
+            this.Text = "Avion";
+            this.Panel1.ResumeLayout(false);
+            this.Panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.Avion)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.Rusia)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.Guatemala)).EndInit();
@@ -161,15 +211,19 @@
 
         #endregion
 
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel Panel1;
         private System.Windows.Forms.PictureBox Guatemala;
         private System.Windows.Forms.PictureBox Rusia;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Control;
         private System.Windows.Forms.PictureBox Avion;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Label Tiempo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label Texto;
+        private System.Windows.Forms.Label TextoDestino;
+        private System.Windows.Forms.Label TextoPrtida;
     }
 }
 
--- Avion/Form1.cs
+++ Avion/Form1.cs
@@ -14,61 +14,151 @@ namespace Avion
 
     public partial class Form1 : Form
     {
-        int Proceso = 1;
-        int X, Y;
+        int Proceso = 0;
+        int X, Y, X1, Y1, X2, Y2, a, b, segundos;
+        double  m, angulo, grados;
+
+        string seleccion;
+
+    
+
+        
 
         public Form1()
         {
             InitializeComponent();
         }
-
         private void panel1_Paint(object sender, PaintEventArgs e)
         {
-         
 
+            Panel1.Controls.Add(Avion);
             
         }
+
 
         private void Guatemala_Click(object sender, EventArgs e)
         {
             //Guatemala.Location.X (Y) obtiene la pocion donde esta el punto rojo
             label1.Text = "X = " + Guatemala.Location.X;
             label2.Text = "Y = " + Guatemala.Location.Y;
-            //solo para poder guardar los datos de una forma mas corta
-            X =  Guatemala.Location.X;
-            Y =  Guatemala.Location.Y;
-            
-            //coloca el Avion el la posicion de Guatemala
-            Avion.Location = new Point(X, Y);
-
-            
+           
+            seleccion = "Guatemala";
         }
 
         private void Rusia_Click(object sender, EventArgs e)
         {
+
             label1.Text = "X = " + Rusia.Location.X;
             label2.Text = "Y = " + Rusia.Location.Y;
-         
+            X2 = Rusia.Location.X;
+            Y2 = Rusia.Location.Y;
 
-            Avion.Location = new Point(X, Y);
+
+            seleccion = "Rusia";
+
+        }
+
+        
+
+        void Calculo()
+        {
+            //calcula el valor de la pendiente
+            a = Y2 - Y1;
+            b = X2 - X1;
+
+            m = ((double)a / (double)b);
+
+            //calculo del algulo
+            angulo = Math.Atan(m);
+
+            //convierte el angulo de radianes a grados solo para mostrarlo
+            grados = angulo * 57.2958;
+
+            //aca se define para que direccion va el avion conforme al angulo y al punto de partida
+            if (X1 > X2)
+            {
+                X = Convert.ToInt32(X1 - segundos * Math.Cos(angulo));
+                Y = Convert.ToInt32(Y1 - segundos * Math.Sin(angulo));
+            }
+
+            if (X1 < X2)
+            {
+                X = Convert.ToInt32(X1 + segundos * Math.Cos(angulo));
+                Y = Convert.ToInt32(Y1 + segundos * Math.Sin(angulo));
+            }
+        }
+
+        void Partida()
+        {
+           if (seleccion == "Guatemala")
+           {
+               X1 = Guatemala.Location.X;
+               Y1 = Guatemala.Location.Y;
+               
+           }
+
+           if (seleccion == "Rusia")
+           {
+               X1 = Rusia.Location.X;
+               Y1 = Rusia.Location.Y;
+           }
+
+           TextoPrtida.Text = "Partida: " + seleccion;
+
+        }
+        void Destino()
+        {
+            if (seleccion == "Guatemala")
+            {
+                X2 = Guatemala.Location.X;
+                Y2 = Guatemala.Location.Y;
+            }
+
+            if (seleccion == "Rusia")
+            {
+                X2 = Rusia.Location.X;
+                Y2 = Rusia.Location.Y;
+            }
+
+            TextoDestino.Text = "Destino: " + seleccion;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             // este proceso puede servir para ingrsar el inicio, destino y accionar el programa de movimiento
+            if (Proceso == 3)
+            {
+                timer1.Start();
+                Texto.Text = "Disfrute el Vuelo";
+               
+            }
+
             if (Proceso == 2)
             {
-                button1.Text = "Vamos!";
+                Destino();
+                Control.Text = "Despegue!";
                 Proceso = 3;
-                timer1.Start();
+
+                Avion.Location = new Point(X1, Y1);
+                
             }
 
             if (Proceso == 1)
             {
-                button1.Text = "Destino";
+                //aca se debera de guardar las coordenadas seleccionadas el los puntos X2 y Y2
+                Control.Text = "Destino";
                 Proceso = 2;
-            } 
+                Texto.Text = "Seleccione el Destino";
+                Partida();
+            }
 
+            if (Proceso == 0)
+            {
+                //aca se debera de guardar las coordenadas seleccionadas el los puntos X1 y Y1
+                Control.Text = "Inicio";
+                Proceso = 1;
+                Texto.Text = "Seleccione un punto de Partida";
+            } 
           
 
         }
@@ -76,23 +166,32 @@ namespace Avion
         private void timer1_Tick(object sender, EventArgs e)
         {
             //Aumenta el valor del laber Tiempo cuando se inica el timer
-            int segundos = Convert.ToInt32(Tiempo.Text);
+            segundos = Convert.ToInt32(Tiempo.Text);
             segundos++;
             Tiempo.Text = Convert.ToString(segundos);
 
-            //esto va a mover el avion conforme a los segundos desde el punto de Guatemala
-            //falta un calculo del angulo para poder hacer que llegue donde esta el destino
-            int X2 = Guatemala.Location.X + segundos;
-            int Y2 = Guatemala.Location.Y - segundos;           
+            Calculo();         
+           
 
-            Avion.Location = new Point(X2, Y2);
+            //cambia la posicion del avion     
+            Avion.Location = new Point(X, Y);
 
-            if (X == Rusia.Location.X && Y == Rusia.Location.Y)
+            //muestra la pocision actual del avion
+            label1.Text = "X = " + Avion.Location.X;
+            label2.Text = "Y = " + Avion.Location.Y;
+
+            //muestra los grados
+            label3.Text = "angulo = " + grados;
+
+            if (X == X2 || Y == Y2)
             {
                 timer1.Stop();
+                Texto.Text = "Gracias por viajar con nosotros";
             }
 
         }
+
+        
 
     }
 }
